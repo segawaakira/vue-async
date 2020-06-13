e1cd679cb7d30b2a871a3959b2d3100b57052877
segawa <>
2020-06-14 01:40:39 +0900

    async awaitで記述

--- vue-ts-practice/src/App.vue
+++ vue-ts-practice/src/App.vue
@@ -2,16 +2,42 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="test()">てすとです</button>
+    <pre>
+      {{ arrayOne }}
+    </pre>
+    <pre>
+      {{ arrayTwo }}
+    </pre>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      arrayOne: [],
+      arrayTwo: []
+    }
+  },
+  methods: {
+    async test() {
+     try {
+       const response1 = await axios.get('https://api.zipaddress.net/?zipcode=6028321');
+       this.arrayOne = response1.data;
+       const response2 = await axios.get(`https://api.zipaddress.net/?zipcode=1670` + response1.data.code);
+       this.arrayTwo = response2.data;
+     } catch(e) {
+       console.log(e);
+     }
+    }
   }
 }
 </script>
